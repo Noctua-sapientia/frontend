d45217c183c4eaef0dba55e4c96b1ea8f8b34e81
Pablo <preinaj@gmail.com>
2023-12-29 10:09:49 +0100

    feat: functional history orders filter added

--- src/components/Orders/HistoryOrders.js
+++ src/components/Orders/HistoryOrders.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import HistoryOrdersFilter from './HistoryOrdersFilter';
 import HistoryOrdersList from './HistoryOrdersList';
 import { Container } from 'react-bootstrap';
@@ -8,121 +8,159 @@ import './HistoryOrders.css';
 function HistoryOrders(props) {
 
   const orders = [
-      {
-        orderId: 1,
-        userId: 1,
-        sellerId: 2,
-        books: [
-          {
-            _id: "658d439a209f46b4655e2c6a",
-            bookId: 2,
-            units: 1
-          }
-        ],
-        status: "In preparation",
-        deliveryAddress: "Reina Mercedes",
-        maxDeliveryDate: "2023-12-21T00:00:00.000Z",
-        creationDatetime: "2023-12-06T11:27:30.647Z",
-        updateDatetime: "2023-12-06T11:27:30.647Z",
-        payment: 23.99
-      },
-      {
-        orderId: 3,
-        userId: 1,
-        sellerId: 2,
-        books: [
-          {
-            _id: "658d439a209f46b4655e2c6b",
-            bookId: 2,
-            units: 1
-          }
-        ],
-        status: "In preparation",
-        deliveryAddress: "Reina Mercedes",
-        maxDeliveryDate: "2023-12-21T00:00:00.000Z",
-        creationDatetime: "2023-12-06T11:35:54.030Z",
-        updateDatetime: "2023-12-06T11:35:54.030Z",
-        payment: 23.99
-      },
-      {
-        orderId: 4,
-        userId: 1,
-        sellerId: 2,
-        books: [
-          {
-            bookId: 2,
-            units: 1,
-            price: 32,
-            _id: "657061f8acf83ad6a5c9f260"
-          }
-        ],
-        status: "In preparation",
-        deliveryAddress: "Reina Mercedes",
-        maxDeliveryDate: "2023-12-21T00:00:00.000Z",
-        creationDatetime: "2023-12-06T11:58:48.628Z",
-        updateDatetime: "2023-12-06T11:58:48.628Z",
-        payment: 23.99
-      },
-      {
-        orderId: 5,
-        userId: 3,
-        sellerId: 2,
-        books: [
-          {
-            bookId: 2,
-            units: 1,
-            price: 6,
-            _id: "657389772e90cd59f7ba4f5d"
-          },
-          {
-            bookId: 1,
-            units: 2,
-            price: 5,
-            _id: "657389772e90cd59f7ba4f5e"
-          }
-        ],
-        status: "In preparation",
-        deliveryAddress: "4 Privet Drive, Little Whinging, Surrey",
-        maxDeliveryDate: "2023-12-25T00:00:00.000Z",
-        creationDatetime: "2023-12-08T21:19:20.210Z",
-        updateDatetime: "2023-12-08T21:24:07.975Z",
-        payment: 16
-      },
-      {
-        orderId: 6,
-        userId: 2,
-        sellerId: 3,
-        books: [
-          {
-            bookId: 5,
-            units: 3,
-            price: 8,
-            _id: "6575bada10f37f5dd014fc80"
-          },
-          {
-            bookId: 6,
-            units: 1,
-            price: 12,
-            _id: "6575bada10f37f5dd014fc81"
-          }
-        ],
-        status: "In preparation",
-        deliveryAddress: "742 Evergreen Terrace, Springfield",
-        maxDeliveryDate: "2023-12-15T00:00:00.000Z",
-        creationDatetime: "2023-12-10T13:19:22.888Z",
-        updateDatetime: "2023-12-10T13:19:22.889Z",
-        payment: 36
-      }
-    ];
-    
+    {
+      orderId: 1,
+      userId: 1,
+      sellerId: 2,
+      books: [
+        {
+          _id: "658d439a209f46b4655e2c6a",
+          bookId: 2,
+          units: 1
+        }
+      ],
+      status: "Sent",
+      deliveryAddress: "Reina Mercedes",
+      maxDeliveryDate: "2023-12-21T00:00:00.000Z",
+      creationDatetime: "2023-12-06T11:27:30.647Z",
+      updateDatetime: "2023-12-06T11:27:30.647Z",
+      payment: 23.99
+    },
+    {
+      orderId: 3,
+      userId: 1,
+      sellerId: 2,
+      books: [
+        {
+          _id: "658d439a209f46b4655e2c6b",
+          bookId: 2,
+          units: 1
+        }
+      ],
+      status: "In preparation",
+      deliveryAddress: "Reina Mercedes",
+      maxDeliveryDate: "2023-12-21T00:00:00.000Z",
+      creationDatetime: "2023-12-06T11:35:54.030Z",
+      updateDatetime: "2023-12-06T11:35:54.030Z",
+      payment: 23.99
+    },
+    {
+      orderId: 4,
+      userId: 1,
+      sellerId: 2,
+      books: [
+        {
+          bookId: 2,
+          units: 1,
+          price: 32,
+          _id: "657061f8acf83ad6a5c9f260"
+        }
+      ],
+      status: "Sent",
+      deliveryAddress: "Reina Mercedes",
+      maxDeliveryDate: "2023-12-21T00:00:00.000Z",
+      creationDatetime: "2023-12-06T11:58:48.628Z",
+      updateDatetime: "2023-12-06T11:58:48.628Z",
+      payment: 23.99
+    },
+    {
+      orderId: 5,
+      userId: 3,
+      sellerId: 2,
+      books: [
+        {
+          bookId: 2,
+          units: 1,
+          price: 6,
+          _id: "657389772e90cd59f7ba4f5d"
+        },
+        {
+          bookId: 1,
+          units: 2,
+          price: 5,
+          _id: "657389772e90cd59f7ba4f5e"
+        }
+      ],
+      status: "In preparation",
+      deliveryAddress: "4 Privet Drive, Little Whinging, Surrey",
+      maxDeliveryDate: "2023-12-25T00:00:00.000Z",
+      creationDatetime: "2023-12-08T21:19:20.210Z",
+      updateDatetime: "2023-12-08T21:24:07.975Z",
+      payment: 16
+    },
+    {
+      orderId: 6,
+      userId: 2,
+      sellerId: 3,
+      books: [
+        {
+          bookId: 5,
+          units: 3,
+          price: 8,
+          _id: "6575bada10f37f5dd014fc80"
+        },
+        {
+          bookId: 6,
+          units: 1,
+          price: 12,
+          _id: "6575bada10f37f5dd014fc81"
+        }
+      ],
+      status: "In preparation",
+      deliveryAddress: "742 Evergreen Terrace, Springfield",
+      maxDeliveryDate: "2023-12-15T00:00:00.000Z",
+      creationDatetime: "2023-12-10T13:19:22.888Z",
+      updateDatetime: "2023-12-10T13:19:22.889Z",
+      payment: 36
+    }
+  ];
 
+  const [filters, setFilters] = useState({
+    orderStatus: '',
+    minPrice: '',
+    maxPrice: '',
+    sortOrder: ''
+  });
+
+  const applyFilters = (orders, filters) => {
+    const { orderStatus, minPrice, maxPrice, sortOrder } = filters;
+
+    let filteredOrders = orders;
+
+    if (orderStatus) {
+      filteredOrders = filteredOrders.filter(order => order.status === orderStatus);
+    }
+
+    if (minPrice) {
+      filteredOrders = filteredOrders.filter(order => order.payment >= parseFloat(minPrice));
+    }
+
+    if (maxPrice) {
+      filteredOrders = filteredOrders.filter(order => order.payment <= parseFloat(maxPrice));
+    }
+
+    if (sortOrder === 'totalPayment') {
+      filteredOrders.sort((a, b) => a.payment - b.payment);
+    } else if (sortOrder === 'deliveryDate') {
+      filteredOrders.sort((a, b) => new Date(a.maxDeliveryDate) - new Date(b.maxDeliveryDate));
+    } else if (sortOrder === 'orderDate') {
+      filteredOrders.sort((a, b) => new Date(a.creationDatetime) - new Date(b.creationDatetime));
+    }
+    return filteredOrders;
+  };
+
+  const filteredOrders = useMemo(() => applyFilters(orders, filters), [orders, filters]);
+
+  const handleFilterChange = (newFilters) => {
+    setFilters(newFilters);
+  };
 
   return (
     <Container>
       <h2 className="section-title">Mi historial de pedidos</h2>
       <div className="section-content">
-        <HistoryOrdersFilter orders={orders}/>
-        <HistoryOrdersList orders={orders}/>
+        <HistoryOrdersFilter onFilterChange={handleFilterChange} />
+        <HistoryOrdersList orders={filteredOrders} />
       </div>
     </Container>
   );
--- src/components/Orders/HistoryOrdersFilter.js
+++ src/components/Orders/HistoryOrdersFilter.js
@@ -1,76 +1,78 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { Card, Button, Form, Collapse } from 'react-bootstrap';
 
-import './HistoryOrders.css';
-
-
-function HistoryOrdersFilter() {
-
+function HistoryOrdersFilter({ onFilterChange }) {
   const [open, setOpen] = useState(false);
+  const [orderStatus, setOrderStatus] = useState('');
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
 
   const inputStyle = { maxWidth: '200px' };
 
+  const handleApplyFilters = () => {
+    onFilterChange({ orderStatus, minPrice, maxPrice, sortOrder });
+  };
+
+  const handleClearFilters = () => {
+    setOrderStatus('');
+    setMinPrice('');
+    setMaxPrice('');
+    setSortOrder('');
+    onFilterChange({ orderStatus: '', minPrice: '', maxPrice: '', sortOrder: '' });
+  };
+
   return (
     <div>
-      <div className="d-flex justify-content-end align-items-end">
-      <Button 
-          variant="outline-info" 
-          onClick={() => setOpen(!open)} 
-          aria-controls="history-orders-filter"
-          aria-expanded={open}
-        >
-          Filtrar y ordenar
-        </Button>
-      </div>
-
+      <Button variant="outline-info" onClick={() => setOpen(!open)} aria-controls="history-orders-filter" aria-expanded={open}>
+        Filtrar y ordenar
+      </Button>
       <Collapse in={open}>
-      <div id="history-orders-filter">
-        <Card>
-          <Card.Body>
-            <Form>
-              <Form.Group className="row mb-3 d-flex align-items-center">
-                <Form.Label className="col-sm-2">Estado del pedido:</Form.Label>
-                <Form.Select className="col" style={inputStyle} id="orderStatus">
-                  <option value="">Elija una opción</option>
-                  <option value="In preparation">In preparation</option>
-                  <option value="Sent">Sent</option>
-                  <option value="Delivered">Delivered</option>
-                  <option value="Cancelled">Cancelled</option>
-                </Form.Select> 
+        <div id="history-orders-filter">
+          <Card>
+            <Card.Body>
+              <Form>
 
-              </Form.Group>
-
-              <Form.Group className="row mb-3 d-flex align-items-center">
-                <Form.Label className="col-sm-2">Rango de precio (€): </Form.Label>
-                <div className='col'>
-                  <div className='row flex-row gap-3'>
-
-                    <Form.Control type="number" style={inputStyle} id="minPrice" placeholder="Mínimo" />
-                    <Form.Control type="number" style={inputStyle} id="maxPrice" placeholder="Máximo" />
-                    
-                  </div>
-                </div>
+                <Form.Group className="row mb-3 d-flex align-items-center">
+                  <Form.Label className="col-sm-2">Estado del pedido:</Form.Label>
+                  <Form.Select className="col" value={orderStatus} onChange={(e) => setOrderStatus(e.target.value)} style={inputStyle}>
+                    <option value="">Elija una opción</option>
+                    <option value="In preparation">In preparation</option>
+                    <option value="Sent">Sent</option>
+                    <option value="Delivered">Delivered</option>
+                    <option value="Cancelled">Cancelled</option>
+                  </Form.Select>
+                </Form.Group>
                 
-              </Form.Group>
+                <Form.Group className="row mb-3 d-flex align-items-center">
+                  <Form.Label className="col-sm-2">Rango de precio (€):</Form.Label>
+                  <div className='col'>
+                    <div className='row flex-row gap-3'>
+                      <Form.Control type="number" min="0" value={minPrice} onChange={(e) => setMinPrice(e.target.value)} style={inputStyle} placeholder="Mínimo" />
+                      <Form.Control type="number" min="0" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} style={inputStyle} placeholder="Máximo" />
+                    </div>
+                  </div>
+                </Form.Group>
 
-              <Form.Group className="row mb-3 d-flex align-items-center">
-                <Form.Label className="col-sm-2">Ordenar por:</Form.Label>
-                <Form.Select className="col" style={inputStyle} id="sortOrder">
-                  <option value="">Elija una opción</option>
-                  <option value="totalPayment">Pago total</option>
-                  <option value="deliveryDate">Fecha prevista de entrega</option>
-                  <option value="orderDate">Fecha de realización</option>
-                </Form.Select> 
-              </Form.Group>
-
-              <div className="d-flex justify-content-center gap-2">
-                <Button variant="info" type="submit">Aplicar filtros</Button>
-                <Button variant="secondary" type="button">Borrar filtros</Button>
-              </div>
-            </Form>
-          </Card.Body>
-        </Card>
-      </div>
+               
+                <Form.Group className="row mb-3 d-flex align-items-center">
+                  <Form.Label className="col-sm-2">Ordenar por:</Form.Label>
+                  <Form.Select className="col" value={sortOrder} onChange={(e) => setSortOrder(e.target.value)} style={inputStyle}>
+                    <option value="">Elija una opción</option>
+                    <option value="totalPayment">Pago total</option>
+                    <option value="deliveryDate">Fecha prevista de entrega</option>
+                    <option value="orderDate">Fecha de realización</option>
+                  </Form.Select>
+                </Form.Group>
+                
+                <div className="d-flex justify-content-center gap-2">
+                  <Button variant="info" onClick={handleApplyFilters}>Aplicar filtros</Button>
+                  <Button variant="secondary" onClick={handleClearFilters}>Borrar filtros</Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </div>
       </Collapse>
     </div>
   );

613bcfccc4438f93a6e95727d9abedc09c075f59
lurodri93 <49364464+lurodri93@users.noreply.github.com>
2024-01-13 18:34:58 +0100

    hotfix alertas erroneas en accounts

--- src/components/User/SellerPage.js
+++ src/components/User/SellerPage.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Button, Row, Col, CardText } from 'react-bootstrap';
-import Alert from '../Alert.js';
 import './user_styles.css'; // Importa tus estilos CSS aquí
 import logoImage from '../../img/logo.png'
 import UserApi from './UserApi.js'
 
 function SellerPage(props) {
-    const [message, setMessage] = useState(null);
     const [user, setUser] = useState([]);
 
     useEffect(() => {
@@ -16,16 +14,13 @@ function SellerPage(props) {
                 setUser(c);
 
             } catch (error) {
-                setMessage(user);
+                
             }
         }
 
         fetchUser();
     });
-    
-    function OnAlertClose(){
-        setMessage(null);
-    }
+
     /*
     function OnUserEdit(user){
         setMessage(user);
@@ -35,7 +30,6 @@ function SellerPage(props) {
         <Container className='home-container'>
             {/* Cabecera */}
             <Col>
-            <Alert message={message} onClose={OnAlertClose}></Alert>
                 <Row>
                     <Col align='center'>
                         <h2>Mi perfil de Vendedor</h2>
